84871a6cc4fc6e47bab851bc7e0ea3fb4c958d5b
StarScreamsss <90149929+StarScreamsss@users.noreply.github.com>
2022-03-31 00:59:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/in', methods=['POST', 'GET'])
 def sign_in():
-    return render_template('registration.html')
-#     return f"""<!DOCTYPE html>
-# <html lang="en">
-# <head>
-#     <meta charset="UTF-8">
-#     <link rel= "stylesheet" type= "text/css" href="static/styles/ver_reg.css">
-#     <title>Title</title>
-# </head>
-# <body>
-#     <div class="form">
-#             <h1 class="tit">Регистрация</h1>
-#             <div class="input-form">
-#                 <input class="login" type="text" placeholder="Придумайте логин">
-#             </div>
-#             <div class="input-form">
-#                 <input class="password" type="password" placeholder="Пароль">
-#             </div>
-#             <div class="input-form">
-#                 <button class="ver">Войти</button>
-#                 <button class="reg">Зарегистрироваться</button>
-#             </div>
-#         </div>
-# </body>
-# </html>"""
+    if request.method == 'GET':
+        return render_template('registration.html')
+    elif request.method == 'POST':
+        print(request.form['login'])
+        print(request.form['password'])
+        return redirect(url_for('home'))
 
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/up', methods=['POST', 'GET'])
 def sign_up():
-    return f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel= "stylesheet" type= "text/css" href="static/styles/ver_reg.css">
-    <title>Title</title>
-</head>
-<body>
-    <div class="form">
-            <h1 class="tit">Вход</h1>
-            <div class="input-form">
-                <input class="login" type="text" placeholder="Логин">
-            </div>
-            <div class="input-form">
-                <input class="password" type="password" placeholder="Пароль">
-            </div>
-            <div class="input-form">
-                <button class="ver">Войти</button>
-                <button class="reg">Зарегистрироваться</button>
-            </div>
-    </div>
-</body>
-</html>"""
+    if request.method == 'GET':
+        return render_template('verification.html')
+    elif request.method == 'POST':
+        print(request.form['login'])
+        print(request.form['password'])
+        return redirect(url_for('home'))
+
+
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':

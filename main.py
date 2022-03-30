b577f91f8ce6dbb20dbe51e58d787c71a8209d54
npocbet <npocbet@gmail.com>
2022-03-30 17:06:17 +0300

    megacommit

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/in', methods=['POST', 'GET'])
 def sign_in():
-    return f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel= "stylesheet" type= "text/css" href="static/styles/ver_reg.css">
-    <title>Title</title>
-</head>
-<body>
-    <div class="form">
-            <h1 class="tit">Регистрация</h1>
-            <div class="input-form">
-                <input class="login" type="text" placeholder="Придумайте логин">
-            </div>
-            <div class="input-form">
-                <input class="password" type="password" placeholder="Пароль">
-            </div>
-            <div class="input-form">
-                <button class="ver">Войти</button>
-                <button class="reg">Зарегистрироваться</button>
-            </div>
-        </div>
-</body>
-</html>"""
+    return render_template('registration.html')
+#     return f"""<!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <link rel= "stylesheet" type= "text/css" href="static/styles/ver_reg.css">
+#     <title>Title</title>
+# </head>
+# <body>
+#     <div class="form">
+#             <h1 class="tit">Регистрация</h1>
+#             <div class="input-form">
+#                 <input class="login" type="text" placeholder="Придумайте логин">
+#             </div>
+#             <div class="input-form">
+#                 <input class="password" type="password" placeholder="Пароль">
+#             </div>
+#             <div class="input-form">
+#                 <button class="ver">Войти</button>
+#                 <button class="reg">Зарегистрироваться</button>
+#             </div>
+#         </div>
+# </body>
+# </html>"""
 
 
 @app.route('/', methods=['POST', 'GET'])

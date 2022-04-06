3e31e255ae087b9c0df2116d01fccb81a70b29cd
StarScreamsss <90149929+StarScreamsss@users.noreply.github.com>
2022-04-07 01:46:41 +0300

    Add files via upload

---
+++ base.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = 'gecr'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+manager = LoginManager(app)
+
+db.create_all()
---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_login import UserMixin
+
+from web_project.base import db, manager
+
+
+class User (db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+
+@manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+

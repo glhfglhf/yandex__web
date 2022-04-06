888c07c2653cd52c24cb8a1929da05003ebf8db2
StarScreamsss <90149929+StarScreamsss@users.noreply.github.com>
2022-04-07 01:47:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,75 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
-app = Flask(__name__)
-
-
-@app.route('/in', methods=['POST', 'GET'])
-def sign_in():
-    if request.method == 'GET':
-        return render_template('registration.html')
-    elif request.method == 'POST':
-        print(request.form['login'])
-        print(request.form['password'])
-        return redirect(url_for('home'))
+from web_project.base import app, db
+from web_project.models import User
 
 
-@app.route('/', methods=['POST', 'GET'])
-@app.route('/up', methods=['POST', 'GET'])
-def sign_up():
-    if request.method == 'GET':
-        return render_template('verification.html')
-    elif request.method == 'POST':
-        print(request.form['login'])
-        print(request.form['password'])
-        return redirect(url_for('home'))
+@app.route('/registration', methods=['POST', 'GET'])
+def registration():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash('Please, fill all fields!')
+        elif password != password2:
+            flash('Passwords are not equal!')
+        else:
+            hash_pwd = generate_password_hash(password)
+            new_user = User(login=login, password=hash_pwd)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login_page'))
+    return render_template('registration.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page)
+        else:
+            flash('Login or password is not correct')
+    else:
+        flash('Please fill login and password fields')
+    return render_template('login.html')
 
 
 @app.route('/home', methods=['POST', 'GET'])
 def home():
     return render_template('home.html')
+
+
+# @app.route('/add_picture', methods=['POST', 'GET'])
+# @login_required
+# def add_picture():
+#     return render_template('add_picture.html')
+
+
+# @app.route('/', methods=['POST', 'GET'])
+# def start():
+#     return render_template('index.html')
+
+
+# @app.route('/logout', methods=['GET', 'POST'])
+# @login_required
+# def logout():
+#     logout_user()
+#     return redirect(url_for('hello_world'))
+
+
+# @app.after_request
+# def redirect_to_signing(response):
+#     if response.status_code == 401:
+#         return redirect(url_for('login_page') + '?next=' + request.url)
+#     return response
 
 
 if __name__ == '__main__':

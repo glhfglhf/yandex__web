4b8e6acbcc4e25b254314d07b25b4370ffb77e31
StarScreamsss <90149929+StarScreamsss@users.noreply.github.com>
2022-04-08 01:07:46 +0300

    Update models.py

--- models.py
+++ models.py
@@ -7,9 +7,9 @@ class User (db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+    avatar = db.Column(db.BLOB, nullable=True)
 
 
 @manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
-
